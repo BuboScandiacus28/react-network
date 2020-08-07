a51dea07a914ece88ca00b4488e176d6585e049b
Alex Korolenko <28filosof28@gmail.com>
2020-08-07 15:07:31 +0300

    Added 'Users' class component, pass ajax request through 'componentDidMount'

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Style from './User.module.css';
+import userPhoto from './Smile.png';
 
 const User = (props) => {
   let checkFollow = (check) => {
@@ -10,12 +11,12 @@ const User = (props) => {
     <div className={Style.root}>
       <div className={Style.left_col}>
         <div className={Style.profile_photo_container}>
-          <img src={props.photoUrl} alt="Фото пользователя"/>
+          <img src={props.photoUrl != null ? props.photoUrl : userPhoto} alt="Фото пользователя"/>
         </div>
         <div onClick={() => props.followEvent(props.userId)} className={Style.follow}>{checkFollow(props.followed)}</div>
       </div>
       <div className={Style.right_col}>
-        <h2 className={Style.full_name}>{props.fullName}</h2>
+        <h2 className={Style.full_name}>{props.name}</h2>
         <div className={Style.location}>
           <h2 className={Style.country}>{props.country},</h2> 
           <h2 className={Style.city}>{props.city}</h2>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,61 +1,35 @@
 import React from 'react';
 import Style from './Users.module.css';
 import User from './User/User';
+import * as axios from 'axios';
 
-const Users = (props) => {
-  if (props.usersDate.length == 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl: 'https://i.pinimg.com/736x/b6/f5/88/b6f588e5b4b9a2625fa5532eaec733d1.jpg',
-        followed: true,
-        fullName: 'Dmirty',
-        status: 'I am a boss',
-        location: {
-          city: 'Kiev',
-          country: 'Ukraine'
-        }
-      },
-      {
-        id: 2,
-        photoUrl: 'https://klike.net/uploads/posts/2019-03/medium/1551512888_2.jpg',
-        followed: false,
-        fullName: 'Maks',
-        status: 'Holla',
-        location: {
-          city: 'Lvive',
-          country: 'Ukraine'
-        }
-      },
-      {
-        id: 3,
-        photoUrl: 'https://i.pinimg.com/736x/70/ae/ed/70aeed769408dede74aaf1818bd3bb32.jpg',
-        followed: true,
-        fullName: 'Pavel',
-        status: 'What are you doing?',
-        location: {
-          city: 'Lugansk',
-          country: 'Ukraine'
-        }
-      },
-    ]);
+class Users extends React.Component {
+
+   //this.usersElements = 
+
+  componentDidMount() {
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        this.props.setUsers(response.data.items);
+      });
   }
 
-  let checkFollowEvent = (check) => {
-    if (!check) return props.follow;
-    else return props.unfollow;
+  checkFollowEvent = (check) => {
+    if (!check) return this.props.follow;
+    else return this.props.unfollow;
   };
 
-  let usersElements = props.usersDate.map(el => {
-    return (<User userId={el.id} photoUrl={el.photoUrl} followed={el.followed} followEvent={checkFollowEvent(el.followed)} fullName={el.fullName} status={el.status} city={el.location.city} country={el.location.country} />);
-  });
+  render() {
+    return (
+      <div className={Style.root}>
+        <h1 className={Style.title}>Пользователи</h1>
+        {this.props.usersDate.map(el => {
+          //debugger;
+          return (<User userId={el.id} photoUrl={el.photos.small} followed={el.followed} followEvent={this.checkFollowEvent(el.followed)} name={el.name} status={el.status} city={"el.location.city"} country={"el.location.country"} />);
+        })}
+      </div>
+    );
+  }
 
-  return (
-    <div className={Style.root}>
-      <h1 className={Style.title}>Пользователи</h1>
-      {usersElements}
-    </div>
-  );
 };
 
 export default Users;
